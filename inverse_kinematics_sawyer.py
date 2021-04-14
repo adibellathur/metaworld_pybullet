bca1fd175488f89b61db04777290b0a887d3c54a
Adithya Bellathur <bellathu@usc.edu>
2021-04-13 20:58:46 -0700

    Fixed Tabs Issue

--- inverse_kinematics_sawyer.py
+++ inverse_kinematics_sawyer.py
@@ -7,47 +7,44 @@ from metaworld_pybullet.sawyer_env import SawyerEnv
 
 def main():
 
-	t=0.
-	LOC_ACC = 0.025
-	prevPose=[0,0,0]
-	prevPose1=[0,0,0]
-	hasPrevPose = 0
-	trailDuration = 15
+    t=0.
+    LOC_ACC = 0.025
+    prevPose=[0,0,0]
+    prevPose1=[0,0,0]
+    hasPrevPose = 0
+    trailDuration = 15
 
-	env = SawyerEnv()
-	obs = env.reset()
+    env = SawyerEnv()
+    obs = env.reset()
 
-	while True:
-		# t=t+0.01
-		# time.sleep(0.01)
-		# env.step()
+    while True:
+        # iterate over these 3 goal positions 
+        goal_pos = [
+            [-0.4, 0.5, 0.4, 0.0],
+            [0.4, 0.5, 0.4, 0.5],
+            [0, 0.5, 0.1, 1.0],
+        ]
+        for goal in goal_pos:
+            while(np.linalg.norm(obs[:3] - goal[:3]) > LOC_ACC):
+                t = t + 0.005
+                time.sleep(0.005)
+                
+                xyz_action = env.convert_to_xyz_action(goal[:3])
+                gripper_action = np.array([goal[3]])
+                action = np.concatenate([xyz_action, gripper_action])
 
-		# iterate over these 3 goal positions 
-		goal_pos = [
-			[-0.4, 0.5, 0.4, 0.0],
-			[0.4, 0.5, 0.4, 0.5],
-			[0, 0.5, 0.1, 1.0],
-		]
-		for goal in goal_pos:
-			while(np.linalg.norm(obs[:3] - goal[:3]) > LOC_ACC):
-				t = t + 0.005
-				time.sleep(0.005)
-				
-				xyz_action = env.convert_to_xyz_action(goal[:3])
-				gripper_action = np.array([goal[3]])
-				action = np.concatenate([xyz_action, gripper_action])
+                obs = env.step(action)
 
-				obs = env.step(action)
+                ls = p.getLinkState(env.body, env.end_effector_index)
+                if (hasPrevPose):
+                    p.addUserDebugLine(prevPose,goal[:3],[0,0,0.3],1,trailDuration)
+                    p.addUserDebugLine(prevPose1,ls[4],[1,0,0],1,trailDuration)
+                prevPose=goal[:3]
+                prevPose1=ls[4]
+                hasPrevPose = 1
 
-				ls = p.getLinkState(env.body, env.end_effector_index)
-				if (hasPrevPose):
-					p.addUserDebugLine(prevPose,goal[:3],[0,0,0.3],1,trailDuration)
-					p.addUserDebugLine(prevPose1,ls[4],[1,0,0],1,trailDuration)
-				prevPose=goal[:3]
-				prevPose1=ls[4]
-				hasPrevPose = 1
+                print("STEP: {}, OBS: {}".format(env.curr_path_length, obs))
 
-				print("STEP: {}, OBS: {}".format(env.curr_path_length, obs))
 
 if __name__ == "__main__":
-	main()
+    main()
